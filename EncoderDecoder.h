5b6c75c767eb64bf5389a595af06ad4e13a37e1d
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2021-11-03 21:29:58 +0900

    update

--- EncoderDecoder.h
+++ EncoderDecoder.h
@@ -1,17 +1,29 @@
+/*
+        @file   EncoderDecoder.h
+        @brief  EncoderReader
+        @author CaseyNelson314
+        @date   2021/11/02
+*/
+
 #include <Arduino.h>
 
 #ifndef DEF_EncoderDecoder_H
 #define DEF_EncoderDecoder_H
-
+#define RpmSelect 0 //rpm 0:整数型,1:浮動小数点型
 class EncoderDecoder {
 
   private:
-
     byte InputPinA, InputPinB;
     byte dataA, dataB;
-    long Countdata;
-    int  Encodata;  //回転方向によって1,-1が入る
-
+    int  Encodata;
+    long Countdata, OldCountdata;
+    unsigned long Time, OldTime;
+    unsigned long pprdata;  // 分解能(クリック/1回転)
+#if !RpmSelect
+    unsigned long Rpmdata;
+#else
+    double Rpmdata;
+#endif
   public:
 
     EncoderDecoder(byte EncoderPinA, byte EncoderPinB): InputPinA(EncoderPinA), InputPinB(EncoderPinB) {
@@ -20,23 +32,52 @@ class EncoderDecoder {
     }
 
     void Update() {
+      OldCountdata = Countdata;
       Countdata += (Encodata = (dataA - dataB) * !(dataA = digitalRead(InputPinA)) * !(dataB = digitalRead(InputPinB)));
+      if (abs(Countdata - OldCountdata) >= 1) {
+        Time = millis();
+#if !RpmSelect
+        Rpmdata = 60000 / ((Time - OldTime) * pprdata);
+#else
+        Rpmdata = 60000 / ((double)(Time - OldTime) * (double)pprdata);
+#endif
+        OldCountdata = Countdata;
+        OldTime = Time;
+      }
     }
 
-    int  GetEncodeData() {
+    int GetEncodeData() {
       return Encodata;
     }
 
     long GetCountData() {
       return Countdata;
     }
-
-    void CountReset() {
-      Countdata = 0;
+#if !RpmSelect
+    unsigned long GetRpm() {
+      Serial.print("hutu");
+      return Rpmdata;
+    }
+#else
+    double GetRpm() {
+      Serial.print("doub");
+      return Rpmdata;
+    }
+#endif
+    void Setppr(unsigned long PPRDATA) {
+      pprdata = PPRDATA;
     }
 
     void SetCountData(long COUNTDATA) {
       Countdata = COUNTDATA;
+    }
+
+    void RpmReset() {
+      Rpmdata = 0;
+    }
+
+    void CountReset() {
+      Countdata = 0;
     }
 
 };
